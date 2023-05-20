9aa9d41777173205a0b15b00c937fca7ffbe42c2
vacuumbeef <stresscult@disroot.org>
2023-05-20 22:27:46 +0300

    merged with my own rootfs-templates from droidian-new-device

--- scripts/setup-system.sh
+++ scripts/setup-system.sh
@@ -6,11 +6,12 @@ echo "127.0.0.1 $1" >> /etc/hosts
 # Generate locales (only en_US.UTF-8 for now)
 sed -i -e '/en_US\.UTF-8/s/^# //g' /etc/locale.gen
 locale-gen
+update-locale LANG=en_US.UTF8
 
 # Change plymouth default theme
 plymouth-set-default-theme mobian
 
 # Load phosh on startup if package is installed
-if [ -f /usr/bin/phosh ]; then
+if [ -f /usr/bin/phosh-session ]; then
     systemctl enable phosh.service
 fi
