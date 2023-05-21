4acce0ed63bcb8a15a895435a035c50e6f95f0be
vacuumbeef <51813411+vacuumbeef@users.noreply.github.com>
2023-05-21 14:36:24 +0300

    Update genimage.sh

--- scripts/genimage.sh
+++ scripts/genimage.sh
@@ -96,27 +96,63 @@ rm -f ${WORK_DIR}/userdata.raw
 
 # Prepare target zipfile
 echo "Preparing zipfile"
-cp -R android-image-flashing-template/template ${WORK_DIR}/target
+if [ ! -d "android-image-flashing-template" ]; then
+    apt update
+    apt install git -y
+    git clone https://github.com/vacuumbeef/android-image-flashing-template
+fi
+echo ${WORK_DIR}
+echo ${ZIP_NAME}
+ls ${WORK_DIR}
+ls ./
+pwd
+mkdir -p ${WORK_DIR}/target/data/
+rm -r android-image-flashing-template/template/data
+cp -R android-image-flashing-template/template/* ${WORK_DIR}/target/
 mv ${WORK_DIR}/userdata.img ${WORK_DIR}/target/data/userdata.img
 
-# Copy kernel and stuff
-bootimage=$(find ${ROOTFS_PATH}/boot -iname boot.img* -type f | head -n 1)
-recovery=$(find ${ROOTFS_PATH}/boot -iname recovery.img* -type f | head -n 1)
-dtbo=$(find ${ROOTFS_PATH}/boot -iname dtbo.img* -type f | head -n 1)
-vbmeta=$(find ${ROOTFS_PATH}/boot -iname vbmeta.img* -type f | head -n 1)
+apt update
+apt install wget -y
+wget https://github.com/vacuumbeef/adaptation-droidian-vayu/releases/download/adaptation/boot.img
+wget https://github.com/vacuumbeef/adaptation-droidian-vayu/releases/download/adaptation/vendor.img
+cp ./boot.img ${WORK_DIR}/target/data/boot.img
+cp ./vendor.img ${WORK_DIR}/target/data/vendor.img
 
-cp "${bootimage}" ${WORK_DIR}/target/data/boot.img
-[ -e "${recovery}" ] && cp "${recovery}" ${WORK_DIR}/target/data/recovery.img
-[ -e "${dtbo}" ] && cp "${dtbo}" ${WORK_DIR}/target/data/dtbo.img
-[ -e "${vbmeta}" ] && cp "${vbmeta}" ${WORK_DIR}/target/data/vbmeta.img
 
-kernel_version=$(basename ${bootimage})
-kernel_version=${kernel_version/boot.img-//}
 
-cat \
-	${ROOTFS_PATH}/usr/lib/flash-bootimage/flash-bootimage.conf \
-	${ROOTFS_PATH}/usr/lib/flash-bootimage/${kernel_version}.conf \
-	> ${WORK_DIR}/target/data/device-configuration.conf
+
+
+
+
+
+
+
+
+
+
+
+
+#cp -R android-image-flashing-template/template ${WORK_DIR}/target
+#mv ${WORK_DIR}/userdata.img ${WORK_DIR}/target/data/userdata.img
+
+## Copy kernel and stuff
+#bootimage=$(find ${ROOTFS_PATH}/boot -iname boot.img* -type f | head -n 1)
+#recovery=$(find ${ROOTFS_PATH}/boot -iname recovery.img* -type f | head -n 1)
+#dtbo=$(find ${ROOTFS_PATH}/boot -iname dtbo.img* -type f | head -n 1)
+#vbmeta=$(find ${ROOTFS_PATH}/boot -iname vbmeta.img* -type f | head -n 1)
+
+#cp "${bootimage}" ${WORK_DIR}/target/data/boot.img
+#[ -e "${recovery}" ] && cp "${recovery}" ${WORK_DIR}/target/data/recovery.img
+#[ -e "${dtbo}" ] && cp "${dtbo}" ${WORK_DIR}/target/data/dtbo.img
+#[ -e "${vbmeta}" ] && cp "${vbmeta}" ${WORK_DIR}/target/data/vbmeta.img
+
+#kernel_version=$(basename ${bootimage})
+#kernel_version=${kernel_version/boot.img-//}
+
+#cat \
+#	${ROOTFS_PATH}/usr/lib/flash-bootimage/flash-bootimage.conf \
+#	${ROOTFS_PATH}/usr/lib/flash-bootimage/${kernel_version}.conf \
+#	> ${WORK_DIR}/target/data/device-configuration.conf
 
 # generate zip
 echo "Generating zip"
