b677752849966b20d9f57b314a43205dc16e76ba
berbascum <berbascum@ticv.cat>
2024-07-13 18:38:35 +0200

    Import from miatool: Fix issue with locales 2.37-16

--- scripts/clean-bootstrap-repositories.sh
+++ scripts/clean-bootstrap-repositories.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Fix issue with locales 2.37-16, from miatooll port
+cat << EOF > /etc/default/locale
+LC_ALL=en_US.UTF-8
+LANG=en_US.UTF-8
+EOF
+
 # Remove extrepos
 rm -f /etc/apt/trusted.gpg.d/droidian-bootstrap.gpg
 rm -f /var/lib/extrepo/keys/vayu.asc
