9a1adacba1e0dbfa79e6e99d664473984b5f42dd
berbascum <berbascum@ticv.cat>
2024-07-13 18:33:51 +0200

    Import from miatool: temporal vayu apt asc and sources deletion after build

--- scripts/clean-bootstrap-repositories.sh
+++ scripts/clean-bootstrap-repositories.sh
@@ -2,6 +2,8 @@
 
 # Remove extrepos
 rm -f /etc/apt/trusted.gpg.d/droidian-bootstrap.gpg
+rm -f /var/lib/extrepo/keys/vayu.asc
+rm -f /etc/apt/sources.list.d/extrepo_vayu.sources
 
 # Nuke /etc/apt/sources.list
 > /etc/apt/sources.list
